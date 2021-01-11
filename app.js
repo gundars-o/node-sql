1551a4bc83ed03ce903ccbfcd127f570760c580b
gundars-o <gundars.ozolins75@gmail.com>
2021-01-11 17:28:34 +0200

    Display created table

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ const env             = require( "./env" );
 const database = new Prohairesis( env.CLEARDB_DATABASE_URL );
 database
     .query(
-        `INSERT INTO User (
-            username,
-            password,
-            date_added
-        ) VALUES (
-            "jake",
-            SHA2( "password", 256 ),
-            NOW()
-        )`
+        `SELECT * FROM User`
     )
     .then( ( res ) => {
             console.log( res );
