5f989ea9e14fa11fbc75db5add51ea109cf7ce6b
gundars-o <gundars.ozolins75@gmail.com>
2021-01-11 17:04:54 +0200

    Create table

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const { Prohairesis } = require( "prohairesis" );
 const env             = require( "./env" );
 const database = new Prohairesis( env.CLEARDB_DATABASE_URL );
+database
+    .query(
+        `CREATE TABLE User(
+            username VARCHAR( 20 ) NOT NULL PRIMARY KEY,
+            password VARCHAR( 300 ) NOT NULL,
+            date_added DATETIME
+        )`
+    )
+    .then( ( res ) => {
+            console.log( res );
+    } )
+    .catch( ( e ) => {
+            console.error( e );
+    } );
